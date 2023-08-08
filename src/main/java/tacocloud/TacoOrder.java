4eb44c2c09c5c34bfacf9f0a2f36e60603f111d3
Hans Christopher Linchpin <misha.sytch@mail.ru>
2023-08-08 17:28:14 +0500

    First

---
+++ src/main/java/tacocloud/TacoOrder.java
@@ -0,0 +1,56 @@
+package tacocloud;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class TacoOrder {
+
+    private static final long serialVersionUID = 1l;
+
+    private Long id;
+
+    private Date placeAt;
+    
+    //deliveryInfo
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryName;
+
+    @NotBlank(message = "Street is required")
+    private String deliveryStreet;
+
+    @NotBlank(message = "City is required")
+    private String deliveryCity;
+
+    @NotBlank(message = "State is required")
+    private String deliveryState;
+
+    @NotBlank(message = "Zip code is required")
+    private String deliveryZip;
+
+    //payment
+    @CreditCardNumber(message = "Not a valid credit card number")
+    private String ccNumber;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+        message = "Must be formatted MM/YY")
+    private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CCV")
+    private String ccCVV;
+    
+    //order
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+}
